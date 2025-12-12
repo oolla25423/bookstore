7ebcd82cdcfa848462ea667701e089c5861588ab
oolla25423 <sema.pivovarov.00@mail.ru>
2025-12-12 09:27:47 +0500

    Fix tests and set DEBUG=False for production

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
